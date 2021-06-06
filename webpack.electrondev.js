8f21f70310b54e4f653db588c75b7480331f9655
Jackson Ming Hu <huming2207@gmail.com>
2021-06-06 18:00:48 +1000

    Add config for electron

---
+++ webpack.electrondev.js
@@ -0,0 +1,25 @@
+/*eslint-disable @typescript-eslint/no-var-requires */
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  devtool: 'inline-source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'esbuild-loader',
+        options: {
+          loader: 'tsx', // Or 'ts' if you don't need tsx
+          target: 'es2016',
+          sourcemap: true,
+          minify: true,
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimize: false,
+  },
+});
