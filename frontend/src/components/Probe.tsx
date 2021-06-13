4f2ef74c5723e09e4c38f66ed2da57d43757388b
Jackson Ming Hu <huming2207@gmail.com>
2021-06-14 09:55:33 +1000

    Code cleanup

--- frontend/src/components/Probe.tsx
+++ frontend/src/components/Probe.tsx
@@ -2,56 +2,33 @@ import { Box } from '@chakra-ui/react';
 import { Stat, StatHelpText, StatLabel, StatNumber } from '@chakra-ui/stat';
 import * as CSS from 'csstype';
 import React from 'react';
+import { ProbeDevice, ProbeStatus } from '../common/ProbeTypes';
 
-export enum ProbeType {
-  DAPLINK = 'DAPLink',
-  STLINK = 'ST-Link',
-}
-
-export enum ProbeState {
-  IDLE = 'Waiting',
-  ERASING = 'Erasing',
-  FLASHING = 'Flashing',
-  SUCCESS = 'Success',
-  ERROR = 'Error',
-}
-
-export interface ProbeStatus {
-  busNumber: number;
-  portNumber: number;
-  type: ProbeType;
-  state: ProbeState;
-  targetChipId?: string;
-}
-
-export interface ProbeStateColor {
-  state: ProbeState;
+export interface ProbeUIColor {
+  status: ProbeStatus;
   color: CSS.Property.Color;
 }
 
-export const Probe = (status: ProbeStatus): JSX.Element => {
-  const stateColorLut: ProbeStateColor[] = [
-    { state: ProbeState.IDLE, color: 'orange' },
-    { state: ProbeState.ERASING, color: 'burlywood' },
-    { state: ProbeState.FLASHING, color: 'lightblue' },
-    { state: ProbeState.SUCCESS, color: 'yellowgreen' },
-    { state: ProbeState.ERROR, color: 'orangered' },
+export const Probe = (params: ProbeDevice): JSX.Element => {
+  const stateColorLut: ProbeUIColor[] = [
+    { status: ProbeStatus.IDLE, color: 'orange' },
+    { status: ProbeStatus.ERASING, color: 'burlywood' },
+    { status: ProbeStatus.FLASHING, color: 'lightblue' },
+    { status: ProbeStatus.SUCCESS, color: 'yellowgreen' },
+    { status: ProbeStatus.ERROR, color: 'orangered' },
   ];
 
   const color = stateColorLut.find((obj) => {
-    return obj.state === status.state;
+    return obj.status === params.status;
   });
-
-  console.log('Color', color);
-  console.log('State', status.state);
 
   return (
     <Box border="1px" boxShadow="xl" borderColor="gray.100" borderRadius="md" bg={color ? color.color : 'white'}>
       <Stat marginLeft={5} marginTop={2}>
-        <StatLabel>{`${status.type} at Bus ${status.busNumber}, Port ${status.portNumber}`}</StatLabel>
-        <StatNumber>{status.state}</StatNumber>
+        <StatLabel>{`${params.info.probeType}; ID: ${params.info.shortId}`}</StatLabel>
+        <StatNumber>{params.status}</StatNumber>
         <StatHelpText>
-          {status.targetChipId ? `Target Chip ID: ${status.targetChipId}` : 'Unknown Chip ID'}
+          {params.targetChipId ? `Target Chip ID: ${params.targetChipId}` : 'Unknown Chip ID'}
         </StatHelpText>
       </Stat>
     </Box>
