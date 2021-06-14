95c47a6c9556bc94acb58cb7239e5035b2f82311
Jackson Ming Hu <huming2207@gmail.com>
2021-06-14 14:51:46 +1000

    frontend: add firmware selection

--- frontend/src/pages/MassProduction.tsx
+++ frontend/src/pages/MassProduction.tsx
@@ -1,13 +1,16 @@
 import { SimpleGrid, VStack } from '@chakra-ui/layout';
-import { Button, ButtonGroup, Container } from '@chakra-ui/react';
+import { Button, ButtonGroup, Container, Stack, Text } from '@chakra-ui/react';
+import { OpenDialogReturnValue } from 'electron';
 import { Observer } from 'mobx-react-lite';
 import React from 'react';
-import { listAllProbes } from '../common/MainProcessBindings';
+import { useState } from 'react';
+import { listAllProbes, openFirmwareChooseDialog } from '../common/MainProcessBindings';
 import { MassProdStat } from '../components/MassProdStat';
 import { Probe } from '../components/Probe';
 import { ProbeStateInstance } from '../states/ProbeState';
 
 export const MassProduction = (): JSX.Element => {
+  const [firmware, setFirmware] = useState<OpenDialogReturnValue>();
   const probeState = ProbeStateInstance;
   return (
     <>
@@ -23,6 +26,35 @@ export const MassProduction = (): JSX.Element => {
           maxW="container.md"
         >
           <MassProdStat />
+        </Container>
+        <Container
+          maxHeight="80px"
+          height="70px"
+          border="2px"
+          paddingTop="3"
+          paddingBottom="3"
+          borderColor="gray.100"
+          borderRadius="md"
+          maxW="container.md"
+        >
+          <Stack direction="row" spacing={5} align="center">
+            <Button
+              colorScheme="teal"
+              variant="solid"
+              onClick={async () => {
+                setFirmware(await openFirmwareChooseDialog());
+              }}
+            >
+              Select firmware blob
+            </Button>
+            <Text fontSize="md">
+              {firmware?.canceled
+                ? 'You have cancelled the selection, try again'
+                : firmware?.filePaths && firmware.filePaths.length > 0
+                ? firmware.filePaths[0]
+                : 'No file selected'}
+            </Text>
+          </Stack>
         </Container>
         <Container
           maxHeight="80px"
